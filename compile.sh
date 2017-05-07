eaa14e8317ba17dc746becb71927abc639bb3016
kijungshin <kijung.shin89@gmail.com>
2017-05-07 09:35:15 -0400

    Fixed a compile error

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@ echo compiling java sources...
 rm -rf class
 mkdir class
 
-javac -cp "./CoreScope-1.0.jar:./library/commons-math3-3.2.jar:./library/fastutil-7.2.0.jar" -d class $(find ./src -name *.java)
+javac -cp "./library/commons-math3-3.2.jar:./library/fastutil-7.2.0.jar" -d class $(find ./src -name *.java)
 
 echo make jar archive...
 cd class
